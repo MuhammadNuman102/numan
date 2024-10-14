7b9e0b44c69bfd7b309fb7ed00861598a283b344
MuhammadNuman102 <numannuman802@gmail.com>
2024-10-14 16:18:26 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>web system assignment</title>
+</head>
+
+<body>
+  <h1>Assignment 3 </h1>
+  <h2>Our term project: Online voting system</h2>
+  <h4>Project Description</h4>
+  <pre>
+In this project we will be using HTML,CSS,Javascript,Php and My SQL.
+This project includes login form, which will get user's mobile number and 
+ password when the user uses this system first time. 
+Also we will be creating a Registration form where the user will enter his details 
+after logging in and also a dashboard for managing all the users and their details
+ will be saved in a database.
+</pre> <br><br>
+<h2>Group members</h2>
+  <?php
+$arr=array('Muhammad shahzaib'=>"BSCS51F22R010",'Muhammad Numan'=>"BSCS51F22R018",'Muhammad Ammar' =>"BSCS51F22R020");
+foreach ($arr as $k=>$v) {
+ echo"$k :$v "."<br>";
+}
+
+  ?>
+
+
+</body>
+
+</html>
